aef8ed079b01df3fe54c5927bb915a8910019bdc
Nathan Caliani <nathan.caliani09@gmail.com>
2025-05-12 16:35:18 -0300

    Frame_baixo posições concluídas
    Todas as posições dos elementos do frame_baixo concluidas.

--- Main.py
+++ Main.py
@@ -15,28 +15,33 @@ def atualizar_lista_tarefas(scroll_frame):  # para limpar e atualizar o scrollab
         # Criando o layout de cada tarefa
         tarefa_frame = ctk.CTkFrame(scroll_frame,
                                     width=550,
-                                    height=100)
+                                    height=120)
         tarefa_frame.pack(pady=5, padx=10, anchor="w")
+        tarefa_frame.grid_propagate(False)
+        tarefa_frame.grid_columnconfigure(1, weight=1)
+        tarefa_frame.grid_columnconfigure(0, weight=1)
+        tarefa_frame.grid_columnconfigure(1, weight=0)
 
         # Nome da tarefa
         nome_label = ctk.CTkLabel(tarefa_frame,
-                                  text=f"Nome: {nome}", font=("Calibri", 16, "bold"))
+                                  text=f" Nome: {nome}",
+                                  font=("Calibri", 18, "bold"),
+                                  wraplength=420,
+                                  justify="left")
         nome_label.grid(row=0, column=0, sticky="w")
 
         # Descrição da tarefa
         descricao_label = ctk.CTkLabel(tarefa_frame,
-                                       text=f"Descrição: {descricao}", font=("Calibri", 14), wraplength=500)
-        descricao_label.grid(row=1, column=0, sticky="w", pady=(5, 0))
+                                       text=f" Descrição: {descricao}",
+                                       font=("Calibri", 16),
+                                       wraplength=550,
+                                       justify="left")
+        descricao_label.grid(row=1, column=0, sticky="w", pady=(5, 0), columnspan=2)
 
         # Adicionando um botão para marcar a tarefa como concluída
         concluir_btn = ctk.CTkButton(tarefa_frame,
                                         text="Concluir Tarefa",
                                         width=100,
-                                        command=lambda t_id=id_: concluir_tarefa(t_id))
-        concluir_btn.grid(row=0, column=1, padx=(10, 0), pady=10)
-
-def concluir_tarefa(tarefa_id):
-
-
-    atualizar_lista_tarefas(scroll_frame)
+                                        font=("Calibri", 16, "bold"))
+        concluir_btn.grid(row=0, column=1, sticky="ne",padx=10, pady=10)
 
